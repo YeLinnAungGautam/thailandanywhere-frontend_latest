9af8f92bfc3ad5bfb55fc68be677bf83c61f49ec
saimain-primary <saimain.primary@gmail.com>
2023-05-30 18:21:42 +0630

    image background

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,7 @@ import { RouterLink, RouterView } from "vue-router";
 <template>
   <main>
     <div class="bg-image bg-with-image font-roboto">
-      <div
-        class="main-content bg-opacity-50 backdrop-filter backdrop-blur-xl"
-      >
+      <div class="main-content bg-opacity-50 backdrop-filter backdrop-blur-xl">
         <RouterView />
       </div>
     </div>
@@ -18,6 +16,6 @@ import { RouterLink, RouterView } from "vue-router";
 .bg-with-image {
   background-repeat: no-repeat;
   background-size: cover;
-  background-image: url("./background.png");
+  background-image: url("/background.png");
 }
 </style>

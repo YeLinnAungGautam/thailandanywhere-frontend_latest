9456d73ff8276bfe4cfdcbdd528d99cffad0a53f
saimain-primary <saimain.primary@gmail.com>
2023-05-31 17:02:24 +0630

    button and input

--- src/views/ComponentView.vue
+++ src/views/ComponentView.vue
@@ -3,6 +3,7 @@ import Layout from "./Layout.vue";
 import Input from "../components/Input.vue";
 import InputField from "../components/InputField.vue";
 import Button from "../components/Button.vue";
+import { EnvelopeIcon } from "@heroicons/vue/24/outline";
 import { ref } from "vue";
 
 const form = ref({
@@ -73,7 +74,74 @@ const form = ref({
     </div>
     <div class="bg-white/60 p-4 rounded-lg shadow-sm mb-5">
       <h4 class="text-gray-600 font-medium mb-3 text-base">Buttons</h4>
-     <Button>Click Here</Button>
+      <div class="space-y-5">
+        <div class="flex items-center gap-3">
+          <Button intent="primary">Primary</Button>
+          <Button intent="secondary">Secondary</Button>
+          <Button intent="warning">Warning</Button>
+          <Button intent="danger">Danger</Button>
+          <Button intent="success">Success</Button>
+          <Button intent="text">Text</Button>
+        </div>
+        <div class="flex items-center gap-3">
+          <Button intent="primary" :left-icon="EnvelopeIcon">Primary</Button>
+          <Button intent="secondary" :left-icon="EnvelopeIcon"
+            >Secondary</Button
+          >
+          <Button intent="warning" :left-icon="EnvelopeIcon">Warning</Button>
+          <Button intent="danger" :left-icon="EnvelopeIcon">Danger</Button>
+          <Button intent="success" :left-icon="EnvelopeIcon">Success</Button>
+          <Button intent="text" :left-icon="EnvelopeIcon">Text</Button>
+        </div>
+        <div class="flex items-center gap-3">
+          <Button intent="primary" :right-icon="EnvelopeIcon">Primary</Button>
+          <Button intent="secondary" :right-icon="EnvelopeIcon"
+            >Secondary</Button
+          >
+          <Button intent="warning" :right-icon="EnvelopeIcon">Warning</Button>
+          <Button intent="danger" :right-icon="EnvelopeIcon">Danger</Button>
+          <Button intent="success" :right-icon="EnvelopeIcon">Success</Button>
+          <Button intent="text" :right-icon="EnvelopeIcon">Text</Button>
+        </div>
+        <div class="flex items-center gap-3">
+          <Button intent="primary" :right-icon="EnvelopeIcon" loading
+            >Primary</Button
+          >
+          <Button intent="secondary" :right-icon="EnvelopeIcon" loading
+            >Secondary</Button
+          >
+          <Button intent="warning" :right-icon="EnvelopeIcon" loading
+            >Warning</Button
+          >
+          <Button intent="danger" :right-icon="EnvelopeIcon" loading
+            >Danger</Button
+          >
+          <Button intent="success" :right-icon="EnvelopeIcon" loading
+            >Success</Button
+          >
+          <Button intent="text" :right-icon="EnvelopeIcon" loading>Text</Button>
+        </div>
+        <div class="flex items-center gap-3">
+          <Button intent="primary" disabled :right-icon="EnvelopeIcon"
+            >Primary</Button
+          >
+          <Button intent="secondary" disabled :right-icon="EnvelopeIcon"
+            >Secondary</Button
+          >
+          <Button intent="warning" disabled :right-icon="EnvelopeIcon"
+            >Warning</Button
+          >
+          <Button intent="danger" disabled :right-icon="EnvelopeIcon"
+            >Danger</Button
+          >
+          <Button intent="success" disabled :right-icon="EnvelopeIcon"
+            >Success</Button
+          >
+          <Button intent="text" disabled :right-icon="EnvelopeIcon"
+            >Text</Button
+          >
+        </div>
+      </div>
     </div>
   </Layout>
 </template>

d4ea852c2e0ed02c31d30951b0ea4a91b7aac952
saimain-primary <saimain.primary@gmail.com>
2023-06-01 23:37:46 +0630

    dashboard and table

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -37,7 +37,7 @@ const testData = {
     </div>
     <div class="grid grid-cols-1 md:grid-cols-4 gap-1 md:gap-4 mb-5">
       <div
-        class="bg-white/60 p-6 rounded-lg shadow-sm mb-5 flex justify-between"
+        class="bg-white/60 p-6 rounded-lg shadow-sm mb-5 flex justify-start gap-5"
       >
         <div class="text-gray-500 shadow-lg p-4 rounded-lg">
           <ArchiveBoxIcon class="w-10 h-10" />
@@ -54,7 +54,7 @@ const testData = {
         </div>
       </div>
       <div
-        class="bg-white/60 p-6 rounded-lg shadow-sm mb-5 flex justify-between"
+        class="bg-white/60 p-6 rounded-lg shadow-sm mb-5 flex justify-start gap-5"
       >
         <div class="text-gray-500 shadow-lg p-4 rounded-lg">
           <CalendarIcon class="w-10 h-10" />
@@ -66,12 +66,12 @@ const testData = {
           <p
             class="text-blue-500 text-2xl font-medium tracking-wide font-roboto"
           >
-            +18
+            +19
           </p>
         </div>
       </div>
       <div
-        class="bg-white/60 p-6 rounded-lg shadow-sm mb-5 flex justify-between"
+        class="bg-white/60 p-6 rounded-lg shadow-sm mb-5 flex justify-start gap-5"
       >
         <div class="text-gray-500 shadow-lg p-4 rounded-lg">
           <UsersIcon class="w-10 h-10" />
@@ -88,7 +88,7 @@ const testData = {
         </div>
       </div>
       <div
-        class="bg-white/60 p-6 rounded-lg shadow-sm mb-5 flex justify-between"
+        class="bg-white/60 p-6 rounded-lg shadow-sm mb-5 flex justify-start gap-5"
       >
         <div class="text-gray-500 shadow-lg p-4 rounded-lg">
           <ArchiveBoxIcon class="w-10 h-10" />

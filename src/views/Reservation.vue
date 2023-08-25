714fd7a3390d103e2ce61a1b9518818ee46b2987
Kaung Myat Kyaw <kaungmyatkyaw18199@gmail.com>
2023-08-25 14:24:23 +0630

    reservation part

--- src/views/Reservation.vue
+++ src/views/Reservation.vue
@@ -15,7 +15,7 @@ import {
   UsersIcon,
   AdjustmentsHorizontalIcon,
 } from "@heroicons/vue/24/outline";
-import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+
 import Swal from "sweetalert2";
 import { useToast } from "vue-toastification";
 import { onMounted, ref, watch } from "vue";
@@ -61,11 +61,12 @@ const searchValue = (val) => {
 
 onMounted(async () => {
   await reservationStore.getListAction();
-  console.log(reservations.value, "this is reservations");
+  console.log(reservations.value.data.length, "this is reservations");
 });
 
 watch(search, async (newValue) => {
   await reservationStore.getListAction({ product_type: search.value });
+  console.log(reservations.value.data.length, "this is reservations");
 });
 </script>
 
@@ -162,12 +163,12 @@ watch(search, async (newValue) => {
         </div>
       </div>
       <div class="overflow-auto rounded-lg shadow mb-5">
-        <div class="grid grid-cols-6 gap-2 bg-gray-100">
-          <div class="text-center text-sm font-medium tracking-wide py-2">
-            No.
-          </div>
+        <div class="grid grid-cols-6 gap-2 bg-gray-200 py-2">
           <div class="text-center text-sm font-medium tracking-wide py-2">
             CRM ID
+          </div>
+          <div class="text-center text-sm font-medium tracking-wide py-2">
+            Product Type
           </div>
           <div class="text-center text-sm font-medium tracking-wide py-2">
             Payment Status
@@ -182,164 +183,103 @@ watch(search, async (newValue) => {
         </div>
         <div
           v-show="!loading"
-          class="divide-y divide-gray-200 group relative"
+          class="group relative"
           v-for="r in reservations?.data"
           :key="r.id"
         >
-          <Disclosure>
-            <DisclosureButton class="py-2 w-full">
-              <div class="grid grid-cols-6 gap-2 bg-white">
-                <div class="p-3 text-sm text-gray-700 whitespace-nowrap">
-                  {{ r.id }}
-                </div>
-
-                <div class="p-3 text-sm text-gray-700 whitespace-nowrap">
-                  {{ r.crm_id }}
-                </div>
-                <div class="p-3 text-sm text-gray-700 whitespace-nowrap">
-                  <p
-                    v-if="r.payment_status == 'fully_paid'"
-                    class="bg-green-500 rounded-full px-3 py-1 inline-block text-white shadow text-xs"
-                  >
-                    {{ r.payment_status }}
-                  </p>
-                  <p
-                    v-if="r.payment_status == 'not_paid'"
-                    class="bg-red-500 rounded-full px-3 py-1 inline-block text-white shadow text-xs"
-                  >
-                    {{ r.payment_status }}
-                  </p>
-                  <p
-                    v-if="r.payment_status == 'partially_paid'"
-                    class="bg-yellow-500 rounded-full px-3 py-1 inline-block text-white shadow text-xs"
-                  >
-                    {{ r.payment_status }}
-                  </p>
-                </div>
-                <div class="p-3 text-sm text-gray-700 whitespace-nowrap">
-                  <p v-if="r.reservation_status != 'null'">
-                    {{ r.reservation_status }}
-                  </p>
-                  <p v-if="r.reservation_status == 'null'">-</p>
-                </div>
-
-                <div class="p-3 text-sm text-gray-700 whitespace-nowrap">
-                  {{ r.booking_date }}
-                </div>
-                <div
-                  class="p-3 text-sm text-gray-700 whitespace-nowrap"
-                  @click="seenClick"
+          <div class="">
+            <div
+              class="grid grid-cols-6 col-span-6 divide-y divide-gray-100 bg-white"
+              v-for="d in r.items"
+              :key="d.id"
+            >
+              <div class="p-3 text-sm mt-2 text-gray-700 whitespace-nowrap">
+                {{ r.crm_id }}
+              </div>
+              <div
+                class="p-3 text-sm mt-2 text-center text-gray-700 whitespace-nowrap"
+              >
+                <p v-if="d.product_type == 'App\\Models\\PrivateVanTour'">
+                  PrivateVanTour
+                </p>
+                <p v-if="d.product_type == 'App\\Models\\GroupTour'">
+                  GroupTour
+                </p>
+                <p v-if="d.product_type == 'App\\Models\\AirportPickup'">
+                  Airpot Pickup
+                </p>
+                <p v-if="d.product_type == 'App\\Models\\EntranceTicket'">
+                  Entrance Ticket
+                </p>
+                <p v-if="d.product_type == 'App\\Models\\Inclusive'">
+                  Inclusive
+                </p>
+              </div>
+              <div
+                class="p-3 text-sm mt-2 text-center text-gray-700 whitespace-nowrap"
+              >
+                <p
+                  v-if="d.payment_status == 'fully_paid'"
+                  class="bg-green-500 rounded-full px-3 py-1 inline-block text-white shadow text-xs"
                 >
-                  <p
-                    class="px-2 py-1 hover:bg-blue-800 cursor-pointer bg-blue-500 inline-block rounded-full text-xs text-white shadow"
+                  {{ d.payment_status }}
+                </p>
+                <p
+                  v-if="d.payment_status == 'not_paid'"
+                  class="bg-red-500 rounded-full px-3 py-1 inline-block text-white shadow text-xs"
+                >
+                  {{ d.payment_status }}
+                </p>
+                <p
+                  v-if="d.payment_status == 'partially_paid'"
+                  class="bg-yellow-500 rounded-full px-3 py-1 inline-block text-white shadow text-xs"
+                >
+                  {{ d.payment_status }}
+                </p>
+              </div>
+              <div
+                class="p-3 text-sm mt-2 text-center text-gray-700 whitespace-nowrap"
+              >
+                <p v-if="d.reservation_status != 'null'">
+                  {{ d.reservation_status }}
+                </p>
+              </div>
+
+              <div
+                class="p-3 text-sm mt-2 text-center text-gray-700 whitespace-nowrap"
+              >
+                {{ d.service_date }}
+              </div>
+              <div class="py-3 pl-10 text-sm text-gray-700 whitespace-nowrap">
+                <div class="flex items-center gap-2">
+                  <router-link
+                    :to="'/reservation/view/' + d.id + '/' + r.crm_id"
                   >
-                    seen<i class="fa-solid fa-chevron-down ml-2"></i>
-                  </p>
+                    <button
+                      class="p-2 text-blue-500 transition bg-white rounded shadow hover:bg-blue-500 hover:text-white"
+                    >
+                      <EyeIcon class="w-5 h-5" />
+                    </button>
+                  </router-link>
+                  <router-link
+                    :to="'/reservation/update/' + d.id + '/' + r.crm_id"
+                  >
+                    <button
+                      class="p-2 text-blue-500 transition bg-white rounded shadow hover:bg-yellow-500 hover:text-white"
+                    >
+                      <PencilSquareIcon class="w-5 h-5" />
+                    </button>
+                  </router-link>
                 </div>
               </div>
-            </DisclosureButton>
-            <transition
-              enter-active-class="transition duration-150 ease-out"
-              enter-from-class="transform scale-95 opacity-0"
-              enter-to-class="transform scale-100 opacity-100"
-              leave-active-class="transition duration-75 ease-out"
-              leave-from-class="transform scale-100 opacity-100"
-              leave-to-class="transform scale-95 opacity-0"
-            >
-              <DisclosurePanel class="text-gray-500 w-full">
-                <div
-                  class="grid grid-cols-6 gap-2 bg-gray-100"
-                  v-for="d in r.items"
-                  :key="d.id"
-                >
-                  <div
-                    class="p-3 text-sm text-center text-gray-700 whitespace-nowrap"
-                  >
-                    {{ d.id }}
-                  </div>
-
-                  <div
-                    class="p-3 text-sm text-center text-gray-700 whitespace-nowrap"
-                  >
-                    <p v-if="d.product_type == 'App\\Models\\PrivateVanTour'">
-                      PrivateVanTour
-                    </p>
-                    <p v-if="d.product_type == 'App\\Models\\GroupTour'">
-                      GroupTour
-                    </p>
-                    <p v-if="d.product_type == 'App\\Models\\AirportPickup'">
-                      Airpot Pickup
-                    </p>
-                    <p v-if="d.product_type == 'App\\Models\\EntranceTicket'">
-                      Entrance Ticket
-                    </p>
-                    <p v-if="d.product_type == 'App\\Models\\Inclusive'">
-                      Inclusive
-                    </p>
-                  </div>
-                  <div
-                    class="p-3 text-sm text-center text-gray-700 whitespace-nowrap"
-                  >
-                    <p
-                      v-if="d.payment_status == 'fully_paid'"
-                      class="bg-green-500 rounded-full px-3 py-1 inline-block text-white shadow text-xs"
-                    >
-                      {{ d.payment_status }}
-                    </p>
-                    <p
-                      v-if="d.payment_status == 'not_paid'"
-                      class="bg-red-500 rounded-full px-3 py-1 inline-block text-white shadow text-xs"
-                    >
-                      {{ d.payment_status }}
-                    </p>
-                    <p
-                      v-if="d.payment_status == 'partially_paid'"
-                      class="bg-yellow-500 rounded-full px-3 py-1 inline-block text-white shadow text-xs"
-                    >
-                      {{ d.payment_status }}
-                    </p>
-                  </div>
-                  <div
-                    class="p-3 text-sm text-center text-gray-700 whitespace-nowrap"
-                  >
-                    <p v-if="d.reservation_status != 'null'">
-                      {{ d.reservation_status }}
-                    </p>
-                  </div>
-
-                  <div
-                    class="p-3 text-sm text-center text-gray-700 whitespace-nowrap"
-                  >
-                    {{ d.service_date }}
-                  </div>
-                  <div
-                    class="py-3 pl-10 text-sm text-gray-700 whitespace-nowrap"
-                  >
-                    <div class="flex items-center gap-2">
-                      <router-link
-                        :to="'/reservation/view/' + d.id + '/' + r.crm_id"
-                      >
-                        <button
-                          class="p-2 text-blue-500 transition bg-white rounded shadow hover:bg-blue-500 hover:text-white"
-                        >
-                          <EyeIcon class="w-5 h-5" />
-                        </button>
-                      </router-link>
-                      <router-link
-                        :to="'/reservation/update/' + d.id + '/' + r.crm_id"
-                      >
-                        <button
-                          class="p-2 text-blue-500 transition bg-white rounded shadow hover:bg-yellow-500 hover:text-white"
-                        >
-                          <PencilSquareIcon class="w-5 h-5" />
-                        </button>
-                      </router-link>
-                    </div>
-                  </div>
-                </div>
-              </DisclosurePanel>
-            </transition>
-          </Disclosure>
+            </div>
+          </div>
+        </div>
+        <div
+          v-if="reservations?.data.length == 0"
+          class="flex justify-center items-center py-20"
+        >
+          Data Empty ...
         </div>
         <div v-if="loading" class="flex justify-center items-center py-20">
           <div

f1625d4971bda53703f81f9c6b12b3647bb0ee09
Sai Main <saimain.primary@gmail.com>
2023-09-05 17:17:27 +0630

    crm id

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -3,7 +3,7 @@
     <!-- Sidebar -->
     <Sidebar />
     <!-- Main -->
-    <div class="w-full h-full bg-[#FFFEFD]">
+    <div class="w-full h-full bg-[#ffede3]">
       <Topbar :title="title" />
       <div class="h-[calc(100vh-80px)] px-[20px] overflow-y-auto py-[20px]">
         <slot></slot>

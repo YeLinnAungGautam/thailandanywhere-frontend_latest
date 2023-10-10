53399415153e5c8cc4e0981df0728d871210b5c9
Sai Main <saimain.primary@gmail.com>
2023-10-10 23:33:45 +0630

    calender

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import "vue-toastification/dist/index.css";
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 import vSelect from "vue-select";
 import "vue-select/dist/vue-select.css";
+import VTooltip from "v-tooltip";
 
 const token = localStorage.getItem("token");
 if (token) {
@@ -27,5 +28,6 @@ app.use(Toast, options);
 
 app.component("v-select", vSelect);
 app.use(createPinia());
+app.use(VTooltip);
 app.use(router);
 app.mount("#app");

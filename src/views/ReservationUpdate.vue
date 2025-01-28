dbe05bd94b0419049c7c8d17dffe394da9fecc33
Kaung Kaung <kaungkaung@Kaungs-Laptop.local>
2025-01-28 10:38:22 +0700

    Kaung

--- src/views/ReservationUpdate.vue
+++ src/views/ReservationUpdate.vue
@@ -1240,8 +1240,8 @@ onMounted(async () => {
             @click="goToReservationImage"
             v-if="
               formData.product_type == 'App\\Models\\EntranceTicket' &&
-              (booking_status.payment_status == 'fully_paid' ||
-                formData.payment_status == 'fully_paid')
+              booking_status.payment_status == 'fully_paid' &&
+              formData.payment_status == 'fully_paid'
             "
           >
             Print As Image

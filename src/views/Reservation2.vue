ee61a106f2613395df1c618ed97cf5e2f156ef47
Kaung Kaung <kaungkaung@Kaungs-Laptop.local>
2025-02-11 08:24:21 +0700

    kaung

--- src/views/Reservation2.vue
+++ src/views/Reservation2.vue
@@ -5,39 +5,55 @@ import Layout from "./Layout.vue";
 import ListReservation from "./Reservation2Component/ListReservation.vue";
 import ReservationCartLoadingVue from "./Dashboard/ReservationCartLoading.vue";
 import { onMounted, ref, watch, computed } from "vue";
-import { ArrowsUpDownIcon, FunnelIcon } from "@heroicons/vue/24/outline";
+import Pagination from "../components/PaginationExpense.vue";
+import {
+  ArrowDownTrayIcon,
+  ArrowsUpDownIcon,
+  FunnelIcon,
+} from "@heroicons/vue/24/outline";
 import ReservationDetail from "./Reservation2Component/ReservationDetail.vue";
 import { useAuthStore } from "@/stores/auth";
 import { useRouter, useRoute } from "vue-router";
+import { useAdminStore } from "../stores/admin";
+import { useHotelStore } from "../stores/hotel";
+import { useEntranceStore } from "../stores/entrance";
 const reservationStore = useReservationStore();
 const { reservations, loading } = storeToRefs(reservationStore);
 const authStore = useAuthStore();
 
 const router = useRouter();
+const adminStore = useAdminStore();
+const hotelStore = useHotelStore();
+const entranceStore = useEntranceStore();
 const route = useRoute();
+const { hotels } = storeToRefs(hotelStore);
+const { entrances } = storeToRefs(entranceStore);
 
 // filter keys
 const limit = ref(10);
 const search = ref("");
 const searchId = ref("");
-const oldCrmId = ref("");
-const bookingStatus = ref("");
-const expenseStatus = ref("");
 const hotel_name = ref("");
 const attraction_name = ref("");
-const customerPaymentStatus = ref("");
-const searchA = ref("");
 const userFilter = ref("");
-const searchReservation = ref("");
 const dateRange = ref(null);
 const searchTime = ref("");
 const empty_unit_cost = ref("");
-const booking_date = ref("");
 const sale_daterange = ref(null);
 const booking_daterange = ref(null);
 const customer_name = ref("");
 const sorting = ref("");
 const dateOnlyToggle = ref(false);
+
+const filterShow = ref(false);
+
+const searchValue = (val) => {
+  search.value = val;
+};
+
+const changePage = async (url) => {
+  await reservationStore.getChangePage(url, watchSystem.value);
+};
 
 // format action
 const formatDate = (datePut) => {
@@ -85,52 +101,17 @@ const watchSystem = computed(() => {
   if (searchId.value != "" && searchId.value != undefined) {
     result.crm_id = searchId.value;
   }
-  if (oldCrmId.value != "" && oldCrmId.value != undefined) {
-    result.old_crm_id = oldCrmId.value;
-  }
-  if (bookingStatus.value != "" && bookingStatus.value != undefined) {
-    result.booking_status = bookingStatus.value;
-  }
-  if (expenseStatus.value != "" && expenseStatus.value != undefined) {
-    result.expense_status = expenseStatus.value;
-  }
   if (hotel_name.value != "" && hotel_name.value != undefined) {
     result.hotel_name = hotel_name.value;
   }
   if (attraction_name.value != "" && attraction_name.value != undefined) {
     result.attraction_name = attraction_name.value;
   }
-  if (
-    customerPaymentStatus.value != "" &&
-    customerPaymentStatus.value != undefined
-  ) {
-    result.customer_payment_status = customerPaymentStatus.value;
-  }
-  if (searchA.value != "" && searchA.value != undefined) {
-    result.filter = searchA.value;
-  }
-  if (searchReservation.value != "" && searchReservation.value != undefined) {
-    result.reservation_status = searchReservation.value;
-  }
-  if (
-    searchTime.value != "" &&
-    searchTime.value != undefined &&
-    dateOnlyToggle.value
-  ) {
+  if (searchTime.value != "" && searchTime.value != undefined) {
     result.service_date = formatDate(searchTime.value);
   }
   if (empty_unit_cost.value != "" && empty_unit_cost.value != false) {
     result.empty_unit_cost = empty_unit_cost.value;
-  }
-  if (
-    booking_date.value != "" &&
-    booking_date.value != undefined &&
-    dateOnlyToggle.value
-  ) {
-    result.booking_date = formatDate(booking_date.value);
-  }
-  if (sale_daterange.value != undefined && !dateOnlyToggle.value) {
-    result.sale_daterange = sale_daterange.value;
   }
   if (booking_daterange.value != undefined && !dateOnlyToggle.value) {
     result.booking_daterange = booking_daterange.value;
@@ -163,13 +144,88 @@ const getDetailAction = async (id) => {
   });
 };
 
+const adminLists = ref([]);
+
+const getListUser = async () => {
+  try {
+    const res = await adminStore.getSimpleListAction();
+    console.log(res, "this is admin list");
+
+    adminLists.value = res.result.data
+      .filter((item) => item.role === "admin")
+      .map((item) => {
+        // Return desired structure or transformation here
+        return {
+          id: item.id,
+          name: item.name,
+        };
+      });
+  } catch (error) {
+    console.log("====================================");
+    console.log(error);
+    console.log("====================================");
+  }
+};
+
 onMounted(async () => {
   if (route.query.id) {
     detailId.value = route.query.id;
   }
   console.log(reservations.value);
   setStartAndEndDate();
+  await getListUser();
 });
+
+const searchCount = computed(() => {
+  let count = 0;
+  if (search.value) {
+    count = count + 1;
+  }
+  if (searchTime.value) {
+    count = count + 1;
+  }
+  if (userFilter.value) {
+    count = count + 1;
+  }
+  if (hotel_name.value) {
+    count = count + 1;
+  }
+  if (attraction_name.value) {
+    count = count + 1;
+  }
+
+  return count;
+});
+
+const adminAction = ref(false);
+const hotelAction = ref(false);
+const entranceAction = ref(false);
+
+watch(
+  [adminAction, hotelAction, entranceAction],
+  async ([newValue, secValue, thridValue]) => {
+    if (newValue == true) {
+      if (admin.value == null) {
+        await adminStore.getSimpleListAction();
+      }
+    }
+    if (secValue == true) {
+      if (hotels.value == null) {
+        await hotelStore.getSimpleListAction();
+      }
+    }
+    if (thridValue == true) {
+      if (entrances.value == null) {
+        await entranceStore.getSimpleListAction();
+      }
+    }
+  }
+);
+
+const searchAction = async () => {
+  filterShow.value = false;
+  await reservationStore.getListAction(watchSystem.value);
+};
 
 watch(dateRange, async (newValue) => {
   console.log(dateRange.value, "this is date");
@@ -203,13 +259,155 @@ watch(dateRange, async (newValue) => {
   <Layout :is_white="true">
     <div class="grid grid-cols-3 gap-4">
       <div class="border shadow-sm rounded-lg p-4">
-        <div class="pb-4 flex justify-start items-center gap-x-3">
+        <div class="pb-4 flex justify-start items-center gap-x-3 relative">
           <div
-            class="bg-blue-600 px-2 rounded-lg shadow py-1 flex justify-center items-center gap-x-2 text-white text-xs"
+            @click="filterShow = !filterShow"
+            class="bg-blue-600 px-2 rounded-lg shadow py-1 flex justify-center items-center gap-x-2 text-white text-xs cursor-pointer"
           >
             <FunnelIcon class="w-5 h-5 text-white" />
             <p>Filter</p>
-            <p>2</p>
+            <p>{{ searchCount }}</p>
+          </div>
+          <div
+            v-if="filterShow"
+            class="absolute top-full py-4 px-4 left-0 w-full transition-all duration-150 bg-white rounded-lg shadow-lg z-50 border border-gray-100 space-y-2"
+          >
+            <p class="text-[10px]">Product Type</p>
+            <div class="">
+              <div
+                class="flex items-center justify-start overflow-scroll no-sidebar gap-1"
+              >
+                <p
+                  class="text-[10px] px-2 whitespace-nowrap cursor-pointer hover:bg-[#ff613c] hover:text-white hover:shadow-md py-1 border border-gray-200 rounded-lg"
+                  @click="searchValue('App\\Models\\PrivateVanTour')"
+                  :class="
+                    search == 'App\\Models\\PrivateVanTour'
+                      ? 'bg-[#ff613c] text-white'
+                      : ''
+                  "
+                >
+                  Private Van tour
+                </p>
+                <p
+                  class="text-[10px] px-2 cursor-pointer hover:bg-[#ff613c] hover:text-white hover:shadow-md py-1 border border-gray-200 rounded-lg"
+                  @click="searchValue('App\\Models\\Hotel')"
+                  :class="
+                    search == 'App\\Models\\Hotel'
+                      ? 'bg-[#ff613c] text-white'
+                      : ''
+                  "
+                >
+                  Hotel
+                </p>
+                <p
+                  class="text-[10px] px-2 cursor-pointer hover:bg-[#ff613c] hover:text-white hover:shadow-md py-1.5 border whitespace-nowrap border-gray-200 rounded-lg"
+                  @click="searchValue('App\\Models\\EntranceTicket')"
+                  :class="
+                    search == 'App\\Models\\EntranceTicket'
+                      ? 'bg-[#ff613c] text-white'
+                      : ''
+                  "
+                >
+                  Entrance Ticket
+                </p>
+                <p
+                  class="text-[10px] px-2 cursor-pointer hover:bg-[#ff613c] hover:text-white hover:shadow-md py-1 border border-gray-200 rounded-lg"
+                  @click="searchValue('App\\Models\\Airline')"
+                  :class="
+                    search == 'App\\Models\\Airline'
+                      ? 'bg-[#ff613c] text-white'
+                      : ''
+                  "
+                >
+                  Airline
+                </p>
+              </div>
+            </div>
+            <p class="text-[10px]">User</p>
+            <select
+              name=""
+              id=""
+              v-model="userFilter"
+              class="px-2 py-1 focus:outline-none border border-gray-300 placeholder-sm bg-white rounded-lg w-3/5 sm:w-3/5 text-[10px] md:w-full text-gray-400 space-y-2 h-9"
+            >
+              <option :value="null" disabled class="bg-gray-200 text-sm">
+                Filter By User
+              </option>
+              <option value="" class="text-[12px]">All User</option>
+              <option
+                :value="key.id"
+                v-for="(key, index) in adminLists"
+                :key="index"
+                class="text-[12px]"
+              >
+                {{ key.name }}
+              </option>
+            </select>
+            <p class="text-[10px]">Service Date</p>
+            <div>
+              <VueDatePicker
+                v-model="searchTime"
+                :format="'yyyy-MM-dd'"
+                placeholder="Service Date"
+                text-input
+              />
+            </div>
+            <p class="text-[10px]">Product Name</p>
+            <div class="flex justify-center items-center">
+              <div
+                v-if="
+                  (!hotels?.data || hotels?.data.length > 10) && !hotelAction
+                "
+                @click="hotelAction = true"
+                class="text-sm text-gray-500 hover:text-gray-600 border border-gray-300 rounded-s-md bg-white px-4 py-1.5 w-full flex justify-between items-center"
+              >
+                <p class="text-[10px]">hotel name</p>
+                <ArrowDownTrayIcon class="w-4 h-4" />
+              </div>
+              <v-select
+                v-if="hotels && hotelAction"
+                class="style-chooser placeholder-sm bg-white rounded-s-lg w-3/5 sm:w-3/5 md:w-full text-gray-400"
+                v-model="hotel_name"
+                :options="hotels?.data"
+                label="name"
+                :clearable="false"
+                :reduce="(d) => d.name"
+              ></v-select>
+              <p
+                @click="hotel_name = ''"
+                class="text-[10px] bg-[#FF613c] px-2 py-2.5 text-white rounded-e-lg"
+              >
+                Clear
+              </p>
+            </div>
+            <div class="flex justify-center items-center">
+              <div
+                v-if="
+                  (!entrances?.data || entrances?.data.length > 10) &&
+                  !entranceAction
+                "
+                @click="entranceAction = true"
+                class="text-sm text-gray-500 hover:text-gray-600 border border-gray-300 rounded-s-md bg-white px-4 py-1.5 w-full flex justify-between items-center"
+              >
+                <p class="text-[10px]">attraction name</p>
+                <ArrowDownTrayIcon class="w-4 h-4" />
+              </div>
+              <v-select
+                class="style-chooser placeholder-sm bg-white rounded-s-lg w-3/5 sm:w-3/5 md:w-full text-gray-400"
+                v-model="attraction_name"
+                v-if="entrances && entranceAction"
+                :options="entrances?.data"
+                label="name"
+                :clearable="false"
+                :reduce="(d) => d.name"
+              ></v-select>
+              <p
+                @click="attraction_name = ''"
+                class="text-[10px] bg-[#FF613c] px-2 py-2.5 text-white rounded-e-lg"
+              >
+                Clear
+              </p>
+            </div>
           </div>
           <div class="bg-white shadow rounded-full border border-gray-100 p-2">
             <ArrowsUpDownIcon class="w-3 h-3" />
@@ -217,14 +415,21 @@ watch(dateRange, async (newValue) => {
           <input
             type="search"
             name=""
+            v-model="searchId"
             placeholder="Search CRM ID"
             class="w-full px-4 py-1.5 rounded-lg shadow border border-gray-100 focus:outline-none text-xs"
             id=""
           />
+          <p
+            class="text-[12px] bg-[#FF613c] px-2 py-1.5 rounded-lg text-white cursor-pointer"
+            @click="searchAction"
+          >
+            Search
+          </p>
         </div>
         <div
           v-if="!loading"
-          class="bg-white shadow rounded-lg divide-y divide-gray-100 max-h-[75vh] overflow-y-scroll"
+          class="bg-white shadow rounded-lg divide-y divide-gray-100 max-h-[69vh] overflow-y-scroll"
         >
           <div
             class=""
@@ -234,6 +439,13 @@ watch(dateRange, async (newValue) => {
           >
             <ListReservation :data="i" :detailId="detailId" />
           </div>
+        </div>
+        <div class="overflow-x-scroll scroll-container py-2">
+          <Pagination
+            v-if="!loading"
+            :data="reservations"
+            @change-page="changePage"
+          />
         </div>
         <div
           v-if="loading"
@@ -252,3 +464,5 @@ watch(dateRange, async (newValue) => {
     </div>
   </Layout>
 </template>
+
+<style scoped></style>
--- src/views/Reservation2Component/ReservationDetail.vue
+++ src/views/Reservation2Component/ReservationDetail.vue
@@ -60,11 +60,11 @@ const hide = ref(false);
     <div class="space-y-6" v-if="!getLoading">
       <div class="flex justify-between items-center">
         <div class="flex justify-start items-center gap-x-2">
-          <p class="text-xs bg-[#FF613c] text-white px-3 py-1.5 rounded-lg">
+          <p class="text-[10px] bg-[#FF613c] text-white px-3 py-1.5 rounded-lg">
             View invoice
           </p>
           <p
-            class="text-xs flex justify-center items-center gap-x-2 text-white px-3 py-1.5 rounded-lg"
+            class="text-[10px] flex justify-center items-center gap-x-2 text-white px-3 py-1.5 rounded-lg"
             :class="{
               'bg-green-500': detail?.payment_status == 'fully_paid',
               'bg-red-500': detail?.payment_status != 'fully_paid',
@@ -77,10 +77,10 @@ const hide = ref(false);
           </p>
         </div>
         <div class="flex justify-end items-center gap-x-2">
-          <p class="text-xs bg-[#FF613c] text-white px-3 py-1.5 rounded-lg">
+          <p class="text-[10px] bg-[#FF613c] text-white px-3 py-1.5 rounded-lg">
             Copy Expense
           </p>
-          <p class="text-xs bg-[#FF613c] text-white px-3 py-1.5 rounded-lg">
+          <p class="text-[10px] bg-[#FF613c] text-white px-3 py-1.5 rounded-lg">
             Generate Confirmation
           </p>
         </div>
@@ -89,7 +89,7 @@ const hide = ref(false);
         <div class="col-span-5 flex justify-between items-center">
           <div>
             <p class="text-[10px] text-gray-500">customer name</p>
-            <p class="text-2xl text-[#FF613c] font-medium pb-2">
+            <p class="text-[22px] text-[#FF613c] font-medium pb-2">
               {{ detail?.customer_info.name }}
             </p>
             <div class="flex justify-start items-center gap-x-2">
@@ -141,15 +141,15 @@ const hide = ref(false);
             </div>
             <div>
               <p class="text-[10px] text-gray-500">Attraction Name</p>
-              <p class="text-base text-[#FF613c] pb-2">
+              <p class="text-[12px] text-[#FF613c] pb-2">
                 {{ detail?.product?.name }}
               </p>
               <p class="text-[10px] text-gray-500">Variation</p>
-              <p class="text-base text-[#FF613c] pb-2">
+              <p class="text-[12px] text-[#FF613c] pb-2">
                 {{ detail?.variation?.name }}
               </p>
               <p class="text-[10px] text-gray-500">Service Date :</p>
-              <p class="text-base text-[#FF613c] pb-2">
+              <p class="text-[12px] text-[#FF613c] pb-2">
                 {{ detail?.service_date }}
               </p>
             </div>
@@ -171,15 +171,15 @@ const hide = ref(false);
             <!-- Rest of the Content -->
             <div>
               <p class="text-[10px] text-gray-500">Total Adult :</p>
-              <p class="text-base text-[#FF613c] pb-2">
+              <p class="text-[12px] text-[#FF613c] pb-2">
                 {{ detail?.quantity }} x {{ detail?.selling_price }}
               </p>
               <p class="text-[10px] text-gray-500">Total Child :</p>
-              <p class="text-base text-[#FF613c] pb-2">-</p>
+              <p class="text-[12px] text-[#FF613c] pb-2">-</p>
               <!-- <p class="text-[10px] text-gray-500">Reser Discount :</p>
-              <p class="text-base text-[#FF613c] pb-2">2380 thb</p> -->
+              <p class="text-[12px] text-[#FF613c] pb-2">2380 thb</p> -->
               <p class="text-[10px] text-gray-500">Reser Expense :</p>
-              <p class="text-base text-[#FF613c]">
+              <p class="text-[12px] text-[#FF613c]">
                 {{ detail?.total_cost_price }} thb
               </p>
               <!-- <p class="text-[10px] text-gray-500">Reser Price</p>
@@ -197,19 +197,19 @@ const hide = ref(false);
             <!-- Rest of the Content -->
             <div>
               <p class="text-[10px] text-gray-500">Reser Discount :</p>
-              <p class="text-base text-[#FF613c] pb-2">
+              <p class="text-[12px] text-[#FF613c] pb-2">
                 {{ detail?.discount }} thb
               </p>
               <!-- <p class="text-[10px] text-gray-500">Payment Method :</p>
-              <p class="text-base text-[#FF613c] pb-2">Cash</p> -->
+              <p class="text-[12px] text-[#FF613c] pb-2">Cash</p> -->
               <!-- <p class="text-[10px] text-gray-500">Total Discount :</p>
-              <p class="text-base text-[#FF613c] pb-2">2380 thb</p> -->
+              <p class="text-[12px] text-[#FF613c] pb-2">2380 thb</p> -->
               <p class="text-[10px] text-gray-500">Total Invoice :</p>
-              <p class="text-base text-[#FF613c] pb-2">
+              <p class="text-[12px] text-[#FF613c] pb-2">
                 {{ detail?.booking?.sub_total }} thb
               </p>
               <p class="text-[10px] text-gray-500">Balance Due</p>
-              <p class="text-base text-[#FF613c]">
+              <p class="text-[12px] text-[#FF613c]">
                 {{ detail?.booking?.balance_due }} thb
               </p>
             </div>
@@ -228,17 +228,17 @@ const hide = ref(false);
               <!-- <p class="text-[10px] text-gray-500">Service Date :</p>
               <p class="text-sm text-[#FF613c] pb-2">02/Feb/2025</p> -->
               <!-- <p class="text-[10px] text-gray-500">Sale Date :</p>
-              <p class="text-base text-[#FF613c] pb-2">08/Feb/2025</p> -->
+              <p class="text-[12px] text-[#FF613c] pb-2">08/Feb/2025</p> -->
               <p class="text-[10px] text-gray-500">Total Items :</p>
-              <p class="text-base text-[#FF613c] pb-2">-</p>
+              <p class="text-[12px] text-[#FF613c] pb-2">-</p>
               <!-- <p class="text-[10px] text-gray-500">Invoice Date :</p>
-              <p class="text-base text-[#FF613c] pb-2">-</p> -->
+              <p class="text-[12px] text-[#FF613c] pb-2">-</p> -->
               <p class="text-[10px] text-gray-500">Score :</p>
-              <p class="text-base text-[#FF613c] pb-2">0.183</p>
+              <p class="text-[12px] text-[#FF613c] pb-2">0.183</p>
               <!-- <p class="text-[10px] text-gray-500">Current Status</p>
-              <p class="text-base text-yellow-400">Booking Request</p> -->
+              <p class="text-[12px] text-yellow-400">Booking Request</p> -->
               <p class="text-[10px] text-gray-500">Payment Method :</p>
-              <p class="text-base text-[#FF613c]">
+              <p class="text-[12px] text-[#FF613c]">
                 {{ detail?.booking?.payment_method }}
               </p>
             </div>
@@ -248,46 +248,70 @@ const hide = ref(false);
       <div>
         <div class="pt-2 relative flex justify-between items-center">
           <!-- line -->
-          <div class="w-full h-[3px] absolute bottom-1.5 bg-gray-200"></div>
+          <div class="w-full h-[3px] absolute bottom-2.5 bg-gray-200"></div>
           <div
-            class="w-4 h-4 rounded-full relative z-10"
-            :class="part == 'general' ? 'bg-[#FF613c]' : 'bg-gray-200'"
-          ></div>
-          <div
-            class="w-36 h-[2px] rounded-full relative z-10 bg-gray-200"
-          ></div>
-          <div
-            class="w-4 h-4 rounded-full relative z-10"
-            :class="part == 'passport' ? 'bg-[#FF613c]' : 'bg-gray-200'"
-          ></div>
+            class="w-6 h-6 flex justify-center items-center text-[10px] rounded-full relative z-10"
+            :class="
+              part == 'general' ? 'bg-[#FF613c] text-white' : 'bg-gray-200'
+            "
+          >
+            1
+          </div>
           <div
             class="w-36 h-[2px] rounded-full relative z-10 bg-gray-200"
           ></div>
           <div
-            class="w-4 h-4 rounded-full relative z-10"
-            :class="part == 'booking' ? 'bg-[#FF613c]' : 'bg-gray-200'"
-          ></div>
+            class="w-6 h-6 flex justify-center items-center text-[10px] rounded-full relative z-10"
+            :class="
+              part == 'passport' ? 'bg-[#FF613c] text-white' : 'bg-gray-200'
+            "
+          >
+            2
+          </div>
           <div
             class="w-36 h-[2px] rounded-full relative z-10 bg-gray-200"
           ></div>
           <div
-            class="w-4 h-4 rounded-full relative z-10"
-            :class="part == 'invoice' ? 'bg-[#FF613c]' : 'bg-gray-200'"
-          ></div>
+            class="w-6 h-6 flex justify-center items-center text-[10px] rounded-full relative z-10"
+            :class="
+              part == 'booking' ? 'bg-[#FF613c] text-white' : 'bg-gray-200'
+            "
+          >
+            3
+          </div>
           <div
             class="w-36 h-[2px] rounded-full relative z-10 bg-gray-200"
           ></div>
           <div
-            class="w-4 h-4 rounded-full relative z-10"
-            :class="part == 'expense' ? 'bg-[#FF613c]' : 'bg-gray-200'"
-          ></div>
+            class="w-6 h-6 flex justify-center items-center text-[10px] rounded-full relative z-10"
+            :class="
+              part == 'invoice' ? 'bg-[#FF613c] text-white' : 'bg-gray-200'
+            "
+          >
+            4
+          </div>
           <div
             class="w-36 h-[2px] rounded-full relative z-10 bg-gray-200"
           ></div>
           <div
-            class="w-4 h-4 rounded-full relative z-10"
-            :class="part == 'confirmation' ? 'bg-[#FF613c]' : 'bg-gray-200'"
+            class="w-6 h-6 flex justify-center items-center text-[10px] rounded-full relative z-10"
+            :class="
+              part == 'expense' ? 'bg-[#FF613c] text-white' : 'bg-gray-200'
+            "
+          >
+            5
+          </div>
+          <div
+            class="w-36 h-[2px] rounded-full relative z-10 bg-gray-200"
           ></div>
+          <div
+            class="w-6 h-6 flex justify-center items-center text-[10px] rounded-full relative z-10"
+            :class="
+              part == 'confirmation' ? 'bg-[#FF613c] text-white' : 'bg-gray-200'
+            "
+          >
+            6
+          </div>
         </div>
         <div class="flex justify-between pt-2 items-center">
           <!-- line -->
@@ -296,11 +320,6 @@ const hide = ref(false);
             @click="part = 'general'"
             :class="part == 'general' ? 'text-[#FF613c]' : ''"
           >
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/4626/4626473.png"
-              class="w-3 h-3 mr-1.5"
-              alt=""
-            />
             Payment
           </div>
           <div
@@ -308,11 +327,6 @@ const hide = ref(false);
             @click="part = 'passport'"
             :class="part == 'passport' ? 'text-[#FF613c]' : ''"
           >
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/11183/11183354.png"
-              class="w-3 h-3 mr-1.5"
-              alt=""
-            />
             Passport
           </div>
           <div
@@ -320,11 +334,6 @@ const hide = ref(false);
             @click="part = 'booking'"
             :class="part == 'booking' ? 'text-[#FF613c]' : ''"
           >
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/10595/10595793.png"
-              class="w-3 h-3 mr-1.5"
-              alt=""
-            />
             Booking
           </div>
           <div
@@ -332,11 +341,6 @@ const hide = ref(false);
             @click="part = 'invoice'"
             :class="part == 'invoice' ? 'text-[#FF613c]' : ''"
           >
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/10595/10595794.png"
-              class="w-3 h-3 mr-1.5"
-              alt=""
-            />
             Invoice
           </div>
           <div
@@ -344,11 +348,6 @@ const hide = ref(false);
             @click="part = 'expense'"
             :class="part == 'expense' ? 'text-[#FF613c]' : ''"
           >
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/10595/10595795.png"
-              class="w-3 h-3 mr-1.5"
-              alt=""
-            />
             Expense
           </div>
           <div
@@ -356,11 +355,6 @@ const hide = ref(false);
             @click="part = 'confirmation'"
             :class="part == 'confirmation' ? 'text-[#FF613c]' : ''"
           >
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/11183/11183358.png"
-              class="w-3 h-3 mr-1.5"
-              alt=""
-            />
             Confirm:
           </div>
         </div>
